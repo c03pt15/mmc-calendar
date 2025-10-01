0421caf178f1520b1a24b6cbe6552eb2493a9ad5
Ghislain Girard <ghislain.girard@hotmail.com>
2025-10-01 18:05:57 -0400

    Make new entry modal larger for better usability

--- src/components/MMCCalendar.tsx
+++ src/components/MMCCalendar.tsx
@@ -648,7 +648,7 @@ const MMCCalendar = () => {
       {/* New Entry Modal */}
       {showNewEntryModal && (
         <div className="fixed inset-0 bg-black bg-opacity-50 flex items-center justify-center z-50">
-          <div className="bg-white rounded-lg p-6 w-96 max-w-full mx-4">
+          <div className="bg-white rounded-lg p-6 w-[600px] max-w-[90vw] mx-4">
             <div className="flex items-center justify-between mb-4">
               <h3 className="text-lg font-medium text-gray-900">New Entry</h3>
               <button
