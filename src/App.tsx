cbd69d58823fa205a6bd1e2b2f770dfae6f8d61b
Ghislain Girard <ghislain.girard@hotmail.com>
2025-10-03 08:48:23 -0400

    Auto-commit before deployment - Fri 10/03/2025  8:48:22.93

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,11 @@
 import React from 'react';
 import MMCCalendar from './components/MMCCalendar';
 
-const App = () => <MMCCalendar />;
+const App = () => (
+  <div>
+    <MMCCalendar />
+    {/* Test deployment - updated via script */}
+  </div>
+);
 
 export default App; 
